881680274c47904176417b9837efc3e5d8fb6718
suisho <suisho000000@gmail.com>
2014-03-23 22:51:48 +0900

    initial commit

---
+++ test/parse.js
@@ -0,0 +1,20 @@
+var parse = require("../parse.js")
+var assert = require("assert")
+describe("",function(){
+  var text = 'color: red; background-color: blue;'
+  var result = parse(text)
+    
+  it("result check", function(){
+    assert.deepEqual(result, {
+      "color": "red",
+      "background-color" : "blue",
+    })
+  })
+  
+  it("sorted", function(){
+    assert.deepEqual(Object.keys(result), [
+      "background-color",
+      "color",
+    ])
+  })
+})
